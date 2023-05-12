db82ab0ac63723dbb347fc8401826f8ccbda3985
Kirill <kirillmaznov@gmail.com>
2023-05-12 13:20:18 +0300

    Modify App.js without jsx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { createElement } from "react";
 
 export const App = () => {
+	//Императивный
 	const currentDate = new Date();
 	const currentYear = currentDate.getFullYear();
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload .
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<h1>{currentYear}</h1>
-			</header>
-		</div>
+	return createElement(
+		"div",
+		{
+			className: "App",
+		},
+		createElement(
+			"header",
+			{
+				className: "App-header",
+			},
+			createElement("img", {
+				src: logo,
+				className: "App-logo",
+				alt: "logo",
+			}),
+			createElement(
+				"p",
+				null,
+				"Edit ",
+				createElement("code", null, "src/App.js"),
+				" and save to reload ."
+			),
+			createElement(
+				"a",
+				{
+					className: "App-link",
+					href: "https://reactjs.org",
+					target: "_blank",
+					rel: "noopener noreferrer",
+				},
+				"Learn React"
+			),
+			createElement("h1", null, currentYear)
+		)
 	);
 };
