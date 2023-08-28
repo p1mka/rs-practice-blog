d2cfcc9d949357aacffcee7c8fb11cc6389eb6bb
p1mka <kirillmaznov@gmail.com>
2023-08-28 18:44:16 +0300

    Настроить проект

--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { createElement } from "react";
+import styled from "styled-components";
+
+const Div = styled.div`
+	text-align: center;
+`;
 
 export const App = () => {
-	//Императивный
-	const currentDate = new Date();
-	const currentYear = currentDate.getFullYear();
-	return createElement(
-		"div",
-		{
-			className: "App",
-		},
-		createElement(
-			"header",
-			{
-				className: "App-header",
-			},
-			createElement("img", {
-				src: logo,
-				className: "App-logo",
-				alt: "logo",
-			}),
-			createElement(
-				"p",
-				null,
-				"Edit ",
-				createElement("code", null, "src/App.js"),
-				" and save to reload ."
-			),
-			createElement(
-				"a",
-				{
-					className: "App-link",
-					href: "https://reactjs.org",
-					target: "_blank",
-					rel: "noopener noreferrer",
-				},
-				"Learn React"
-			),
-			createElement("h1", null, currentYear)
-		)
+	return (
+		<Div>
+			<i class="fa fa-camera-retro fa-5x" />
+		</Div>
 	);
 };
