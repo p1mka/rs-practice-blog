bebb166e45ad5305a062b9a3346463360f49f018
p1mka <kirillmaznov@gmail.com>
2023-09-01 10:52:49 +0300

    Реализовать шапку

---
+++ src/components/header/components/logo/logo.js
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { Icon } from "../../../../components";
+
+const LargeText = styled.div`
+	font-size: 40px;
+	font-weight: 600;
+	line-height: 48px;
+`;
+const SmallText = styled.div`
+	margin-top: -5px;
+	font-size: 18px;
+	font-weight: bold;
+`;
+
+const LogoContainer = ({ className }) => (
+	<Link className={className} to="/">
+		<Icon id="fa-code" size="70px" margin="0 10px 0 0" />
+		<div>
+			<LargeText>Блог</LargeText>
+			<SmallText>веб-разработчика</SmallText>
+		</div>
+	</Link>
+);
+
+export const Logo = styled(LogoContainer)`
+	display: flex;
+	margin-top: -6px;
+`;
---
+++ src/components/header/header.js
@@ -0,0 +1,27 @@
+import { ControlPanel, Logo } from "./components";
+import styled from "styled-components";
+
+const Description = styled.div`
+	font-style: italic;
+`;
+
+const HeaderContainter = ({ className }) => (
+	<header className={className}>
+		<Logo />
+		<Description>
+			Веб-технологии <br /> Написание кода <br /> Разбор ошибок
+		</Description>
+		<ControlPanel />
+	</header>
+);
+export const Header = styled(HeaderContainter)`
+	display: flex;
+	justify-content: space-between;
+	position: fixed;
+	top: 0;
+	width: 1000px;
+	background-color: #fff;
+	height: 120px;
+	padding: 20px 40px;
+	box-shadow: -1px 5px 6px 0px rgba(135, 135, 135, 1);
+`;
